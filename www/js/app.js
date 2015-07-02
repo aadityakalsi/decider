74f45755c34c4bf55ac438ca1c6579cae8bc4c89
Aaditya Kalsi <akalsi.dev@gmail.com>
2015-07-01 22:26:11 -0400

    Adding refresh and fake chats

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,17 @@
 var app = angular.module("decider", ["ionic"]);
 
 var decider_ctrl = function ($scope, $log) {
+    $scope.chats = [];
     $scope.show_chats = function() {
         $log.info("show_chats() called");
+        var date = new Date();
+        $scope.chats.unshift({
+            author: "Adi",
+            time: date.getTime(),
+            question: "Short question",
+            detail_question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, ipsam. Iure dignissimos corporis, consequuntur minus sint expedita vitae ipsum ex consectetur adipisci impedit repellendus explicabo id aliquam saepe, nihil deserunt."
+        });
+        $scope.$broadcast("scroll.refreshComplete");
     }
 };
 
